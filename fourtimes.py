8f0868dc5e1e4a8ead4ebc10ee2a359576dad126
Hakan <hakan_ak@hotmail.co.uk>
2021-11-11 16:09:26 +0000

    Add files via upload

---
+++ fourtimes.py
@@ -0,0 +1,10 @@
+# Multiplication table from 0 to 12
+
+num = 4
+
+# To take input from the user - e.g type 5 to get five times table.
+# num = int(input("Display multiplication table of? "))
+
+# Iterate 10 times from i = 0 to 12 - Change the value 13 to multiply bigger numbers!!!
+for i in range(0, 13):
+   print(num, 'x', i, '=', num*i)
